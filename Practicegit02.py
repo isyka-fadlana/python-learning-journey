ea0f97c8f3310d40e188a308bae6505d0c5a3edd
Isyka <muhl75700@gmail.com>
2026-06-12 19:02:09 +0700

    Update practice.py - add club variable

--- Practicegit02.py
+++ Practicegit02.py
@@ -3,3 +3,6 @@ name = "Isyka Fadlana"
 goal = "Become World-Class AI Systems Engineer"
 print(f"Hello, {name}!")
 print(f"Goal: {goal}")
+
+club = "Future World-Class Engineer Club"
+print(f"Club: {club}")
