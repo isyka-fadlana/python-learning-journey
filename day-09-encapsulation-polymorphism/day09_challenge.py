600019c962d32fa8cf1e9904e1269573c64667c5
Muh Isyka Fadlana <misykaf@Muhs-MacBook-Pro.local>
2026-07-29 15:43:31 +0700

    Complete Day 09: Encapsulation and Polymorphism

--- day-09-encapsulation-polymorphism/day09_challenge.py
+++ day-09-encapsulation-polymorphism/day09_challenge.py
@@ -6,8 +6,36 @@ class Employee:
         print(f"Your salary {self.__salary}")
 
     def give_raise(self, amount):
-        self.__salary += amount
-        if amount < self.__salary:
-            print("Raise can't negative")
-        elif amount > self.__salary:
-            print(f"your salary is raise {self.__salary}")
+        if amount < 0:
+            print("Raise can't be negative")
+        else:
+            self.__salary += amount
+            print(f"Your salary is raised to {self.__salary}")
+
+employee = Employee(5000)
+employee.get_salary()
+employee.give_raise(500)
+employee.get_salary()
+
+class Shape:
+    def area(self):
+        print("Area not defined")
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+shapes =[Square(1), Circle(1)]
+
+for shape in shapes:
+        print(shape.area())
